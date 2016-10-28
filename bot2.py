5d58926962162b04531df8ea648361aa80b8a283
Pernath <cgah.95@gmail.com>
2016-10-28 18:16:06 -0500

    botodes

--- bot2.py
+++ bot2.py
@@ -18,7 +18,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://maubot.herokuapp.com/bot")
+    bot.set_webhook(url="https://maubot.herokuapp.com/")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
