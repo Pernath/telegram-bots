32aa3aa898ad3ff02e8a8e01c3a9e74b6318b1cf
Pernath <cgah.95@gmail.com>
2017-03-25 00:30:16 -0600

    Prueba2

--- bot2.py
+++ bot2.py
@@ -30,7 +30,7 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['[a-zA-Z]*'])
 def send_gif(message):
-    if 'nudes' in message and message.chat.title == "Primitivo":
+    if 'nudes' in message:
         bot.reply_to(message, "Ese bot ShowNudes me la pela... Estoy tratando de conseguir material más nuevo y primitivo para ti... :v")
         bot.send_document(message.chat.id, choose_gif())
 
